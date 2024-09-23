47dd8989af3dae170798c09c417cff1948c8bdc1
Chitra Rajendran <chitra.devi@teliacompany.com>
2024-09-23 22:52:00 +0200

    Log File has been added

--- src/main/java/com/inter/ikea/product/config/WebConfig.java
+++ src/main/java/com/inter/ikea/product/config/WebConfig.java
@@ -4,13 +4,22 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.beans.factory.annotation.Value;
+
 
 @Configuration
 @EnableWebMvc
 public class WebConfig implements WebMvcConfigurer {
 
+    @Value("${cors.allowed-origins}")
+    private String[] allowedOrigins;
+
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**").allowedOrigins("http://localhost:3000","http://localhost");
+        registry.addMapping("/**")
+        .allowedOrigins(allowedOrigins)
+        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+        .allowCredentials(true);
+
     }
 }
